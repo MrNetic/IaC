d07b7b37492f489fc600663e4da42d9fbad135b3
Paulo Condeça <paulo@citen.net>
2017-06-06 20:44:22 +0100

    scripts

--- AzurePreBuiltEnvironmentSQLServer_HA_DR/2_CreateSubnets.ps1
+++ AzurePreBuiltEnvironmentSQLServer_HA_DR/2_CreateSubnets.ps1
@@ -1,6 +1,10 @@
-#Create 2 different Subnets in Dublin, one where the DCs are and another one for SQL Server
+#Create The Virtual Network in Dublin
+New-AzureRmVirtualNetwork -ResourceGroupName rgInfrastructure01 -Name VnetProductionDublin -AddressPrefix 192.168.0.0/16 -Location northeurope
+
+#Create 2 different Subnets in Dublin to segment the workloads, one for the DCs and another one for SQL Server
 $subnetDCs= New-AzureRmVirtualNetworkSubnetConfig -Name subnetProductionDublin -AddressPrefix 192.168.111.0/24
 $subnetSQLs= New-AzureRmVirtualNetworkSubnetConfig -Name subnetProductionDublin -AddressPrefix 192.168.112.0/24
+
 
 #Create VNet in Dublin with the 2 subnets specified above
 New-AzureRmVirtualNetwork -ResourceGroupName rgInfrastructure01 -Name VnetProductionDublin -AddressPrefix 192.168.0.0/16 -Location northeurope -Subnet $subnetDCs
